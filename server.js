9c292659c1db43c4d720b5be106930040fe8e944
Asmovic <asogba007@gmail.com>
2020-11-18 19:21:01 +0100

    Expose public path

--- server.js
+++ server.js
@@ -11,10 +11,14 @@ const app = express();
 // the __dirname is the current directory from where the script is running
 app.use(express.static('dist'));
 
-/* app.use(express.static(`${__dirname}/public`)); */
+app.use(express.static(`${__dirname}/public`));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 // send the user to index html page inspite of the url
-app.get('*', (req, res) => {
+/* app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-});
+}); */
 app.listen(PORT, () => console.log(`Server is up running on ${PORT}`));
