5b2417690f9d67e3e66e60f39f8a664324816d9e
Asmovic <asogba007@gmail.com>
2020-11-22 09:00:04 +0100

    Clean up

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,26 +2,14 @@
 /* eslint-disable import/no-named-as-default */
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-/* import { GlobalStyle } from '../global.styles';
-import Header from '../components/header/header.component';
-import NavBar from '../components/nav-bar/nav-bar.component';
-import Footer from '../components/footer/footer.component'; */
 import HomePage from '../Pages/HomePage';
 
 function Routes() {
   return (
     <BrowserRouter>
-
-      {/*         <GlobalStyle />
-        <Header />
-        <NavBar /> */}
-
       <Switch>
         <Route path="/" exact component={HomePage} />
-
       </Switch>
-      {/* <Footer /> */}
-
     </BrowserRouter>
   );
 }
